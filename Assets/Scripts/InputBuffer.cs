f9f0408048d7bcdf682d3c7bd4ec84de7c765880
Aramcho <osipyan.aram2017@ya.ru>
2021-04-10 00:21:05 +0300

    add environment for level part4

---
+++ Assets/Scripts/InputBuffer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace Assets.Scripts
+{
+    class InputBuffer
+    {
+        int size;
+        StringBuilder buffer;
+        float lasttime;
+        public float LastTime
+        {
+            get
+            {
+                return lasttime;
+            }
+        }
+        public InputBuffer(int size)
+        {
+            this.size = size;
+            buffer = new StringBuilder(size+1);
+            lasttime = Time.time;
+        }
+        public void AddBuffer(char input)
+        {
+            if (input != 'A' && input != 'D')
+            {
+                return;
+            }
+            buffer.Append(input);
+            if(buffer.Length > size)
+            {
+                Debug.Log("Rem");
+                buffer.Remove(0, 1);// remove the first elem
+            }
+            lasttime = Time.time;
+        }
+        public void Clear()
+        {
+            Debug.Log("Rem");
+            buffer.Clear();
+        }
+        public Combination CheckBuffer(List<Combination> combinations)
+        {
+            string buffer = this.buffer.ToString();
+            foreach (var item in combinations)
+            {
+                if (buffer.Contains(item.ComboName))
+                {
+                    this.buffer.Clear();
+                    return item;
+                }
+                
+            }
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+using System.Text;
+public class InputController : MonoBehaviour
+{
+    [SerializeField]
+    Animator animator;
+
+    List<Assets.Scripts.Combination> combinations;
+    InputBuffer inputBuffer;
+    void Start()
+    {
+        combinations = new List<Combination>();
+        AddCombination();
+        inputBuffer = new InputBuffer(5);
+    }
+
+    void AddCombination(string comboName = "AA")
+    {
+        combinations.Add(new Combination(5,comboName));
+    }
+    void Update()
+    {
+        ProcessInput();
+    }
+    void ProcessInput()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //animator.SetTrigger("A");
+            inputBuffer.AddBuffer('A');
+            Debug.Log('A');
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //animator.SetTrigger("D");
+            inputBuffer.AddBuffer('D');
+            Debug.Log('D');
+        }
+        if (Time.time - inputBuffer.LastTime>1f)
+        {
+            inputBuffer.Clear();
+        }
+        Combination currenCombination = inputBuffer.CheckBuffer(combinations);
+        if (currenCombination!= null)
+        {
+            Debug.Log("Found combination - " + currenCombination.ComboName);
+        }
+    }
+}
