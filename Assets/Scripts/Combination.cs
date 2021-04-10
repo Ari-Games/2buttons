a28163ca1d5b1d373edb481161e566c9a4c57125
FazilovDev <fazilov.99@mail.ru>
2021-04-10 22:40:17 +0300

    Update

---
+++ Assets/Scripts/Combination.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    class Combination
+    {
+        string comboName;
+        int damage;
+        public Combination()
+        {
+
+        }
+        public Combination(int _damage,string _comboName)
+        {
+            damage = _damage;
+            comboName = _comboName;
+        }
+        public string ComboName
+        {
+            get
+            {
+                return comboName;
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+using System.Text;
+using System;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField]
+    Animator animator;
+
+    List<Assets.Scripts.Combination> combinations;
+    InputBuffer inputBuffer;
+    System.Random random = new System.Random();
+    void Start()
+    {
+        combinations = new List<Combination>();
+        AddCombination();
+        AddCombination("ADA");
+        inputBuffer = new InputBuffer(5);
+    }
+
+    public void AddCombination(string comboName = "AA",int damage = 5)
+    {
+        combinations.Add(new Combination(damage,comboName));
+    }
+    void Update()
+    {
+        ProcessInput();
+    }
+    void ProcessInput()
+    {
+        char? keyCode = null;
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //animator.SetTrigger("A");
+            keyCode = 'A';
+            inputBuffer.AddBuffer('A');
+            Debug.Log('A');
+        } else
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //animator.SetTrigger("D");
+            keyCode = 'D';
+            inputBuffer.AddBuffer('D');
+            Debug.Log('D');
+        }
+        if (Time.time - inputBuffer.LastTime>1f)
+        {
+            inputBuffer.Clear();
+            //FirstLevelHit(keyCode);
+        }
+        Combination currenCombination = inputBuffer.CheckBuffer(combinations);
+        if (currenCombination!= null)
+        {
+            Debug.Log("Found combination - " + currenCombination.ComboName);
+            HitByCombination(currenCombination);
+            return;
+        }
+        
+       
+    }
+
+    private void HitByCombination(Combination combination)
+    {
+        animator.SetTrigger(combination.ComboName.Length + "_" + random.Next(1, 3));
+    }
+
+    void FirstLevelHit(char? keyCode)
+    {
+        if (keyCode!=null)
+        {
+            animator.SetTrigger(keyCode.ToString());
+        }
+    }
+}
