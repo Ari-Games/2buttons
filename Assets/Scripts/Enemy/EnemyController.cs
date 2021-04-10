6d7c18a85f1d1e70e2064421c2a6da043138f902
FazilovDev <fazilov.99@mail.ru>
2021-04-10 22:29:27 +0300

    update

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform Target;
+    [HideInInspector] public EnemyMovement Movement;
+
+    private IEnumerator Start()
+    {
+        yield return null;
+        Movement = GetComponent<EnemyMovement>();
+        Debug.Log(Target.position);
+        Movement.To(Target.position);
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMovement : MonoBehaviour
+{
+    public float Speed;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = Speed;
+    }
+
+    public void To(Vector3 point)
+    {
+        agent.SetDestination(point);
+    }
+
+}
